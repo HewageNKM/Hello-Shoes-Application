f290d68a9ab8ab99549e0be90139cbf78983a634
Nadun Kawishika <hnkmalwenna@gmail.com>
2024-04-23 21:54:43 +0530

    Login Complete

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+$('#loginForm').submit(function (e) {
+    e.preventDefault();
+    const email = e.target.email.value.toString();
+    const password = e.target.password.value.toString();
+
+    console.log("Email: " + email, "Password: " + password);
+
+    if (password.trim().length < 8) {
+        $("#passwordFld").addClass("border-2 border-red-500");
+        return;
+    } else {
+        $("#passwordFld").removeClass("border-2 border-red-500");
+    }
+    $.ajax("http://localhost:8080/api/v1/auth/login", {
+        method:"POST",
+        contentType: "application/json",
+        allowOrigin: "*",
+        data: JSON.stringify({
+            email:email,
+            password:password
+        }),
+        success: function (data) {
+            console.log(data);
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("role", data.role[0].authority);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+
+});
