9407132b54409cf8e499d229ec1e82779ea6ed2e
Nadun Kawishika <hnkmalwenna@gmail.com>
2024-05-15 20:16:22 +0530

    stock refresh and search

--- public/assets/js/stock.js
+++ public/assets/js/stock.js
@@ -1,6 +1,45 @@
-let stocksList = [];
-const loadStockTable = () => {
+const stockTableLoadingAnimation = $("#stockTableLoadingAnimation");
+const stockSuccessMessage = $("#success");
+const stockAlertMessage = $("#alert");
 
+let stocksList = [];
+
+$("#stockSearchBtn").click(function (e) {
+    searchStocks()
+})
+$("#stocksRefreshBtn").click(function (e) {
+    loadStockTable()
+})
+const searchStocks = () => {
+    const value = $("#stockSearchFld").val();
+    if (value === "") {
+        setStockAlertMessage("Please enter a value to search")
+        return;
+    }
+    stockTableLoadingAnimation.removeClass("hidden");
+    stockTableLoadingAnimation.addClass("flex");
+    $.ajax({
+        url: BASEURL + "/inventory/items/stocks/filter/" + value,
+        method: "GET",
+        processData: false,
+        contentType: false,
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem("token")
+        },
+        success: function (res) {
+            stocksList = res;
+            stockTableLoadingAnimation.removeClass("flex");
+            stockTableLoadingAnimation.addClass("hidden");
+            setStockTableContent();
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    })
+}
+const loadStockTable = () => {
+    stockTableLoadingAnimation.removeClass("hidden");
+    stockTableLoadingAnimation.addClass("flex");
     $.ajax({
         url: BASEURL + "/inventory/items/stocks",
         method: "GET",
@@ -12,6 +51,8 @@ const loadStockTable = () => {
         success: function (res) {
             stocksList = res;
             setStockTableContent();
+            stockTableLoadingAnimation.removeClass("flex");
+            stockTableLoadingAnimation.addClass("hidden");
         },
         error: function (error) {
             console.log(error);
@@ -19,8 +60,9 @@ const loadStockTable = () => {
     })
 
 }
+
 const setStockTableContent = () => {
-  $("#stockTableBody").empty();
+    $("#stockTableBody").empty();
     stocksList.forEach((stock) => {
         $("#stockTableBody").append(
             `<tr class="odd:bg-white even:bg-gray-50 hover:bg-blue-200 font-light" id="${stock.stockId}">
@@ -36,10 +78,29 @@ const setStockTableContent = () => {
                         <td class="m-1 p-2 "><input class="w-[5rem] bg-slate-100 p-2 rounded-md" type="number" value="${stock.size44}"></td>
                         <td class="m-1 p-2 "><input class="w-[5rem] bg-slate-100 p-2 rounded-md" type="number" value="${stock.size45}"></td>
                         <td class="m1- p-2">
-                            <button value="${stock.stockId}" id="itemEditBtn" class="text-blue-600 font-bold m-1 p-1 hover:border-b-2 border-blue-600">Update</button>
+                            <button value="${stock.stockId}" id="stockEditBtn" class="text-blue-600 font-bold m-1 p-1 hover:border-b-2 border-blue-600">Update</button>
                         </td>
             </tr>`
         )
     })
+}
+const setStockSuccessMessage = (message) => {
+    $("#successDescription").text(message)
+    stockSuccessMessage.removeClass("right-[-100]")
+    stockSuccessMessage.addClass("right-[0]")
+    setTimeout(() => {
+        stockSuccessMessage.addClass("right-[-100]")
+        stockSuccessMessage.removeClass("right-[0]")
+    }, 5000)
+}
+
+const setStockAlertMessage = (message) => {
+    $("#alertDescription").text(message)
+    stockAlertMessage.removeClass("right-[-100]")
+    stockAlertMessage.addClass("right-[0]")
+    setTimeout(() => {
+        stockAlertMessage.addClass("right-[-100]")
+        stockAlertMessage.removeClass("right-[0]")
+    }, 5000)
 }
 loadStockTable();
